f0e9065a433ee64b01b8ccbcffd92793a7ef0119
Jakob Prufer <jakobo.pruefer@gmail.com>
2023-11-13 19:56:08 +0000

    started on screens section

---
+++ src/app/components/SixthLighter.tsx
@@ -0,0 +1,20 @@
+"use client";
+import { useRef } from "react";
+import { useScroll, motion } from "framer-motion";
+
+export default function SixthLighter() {
+  //transition seventhSection
+  const seventh = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: seventh,
+    offset: ["0 0", "0.8 1"],
+  });
+
+  return (
+    <motion.div
+      className="sixthSectionLighter"
+      ref={seventh}
+      style={{ opacity: scrollYProgress }}
+    ></motion.div>
+  );
+}
